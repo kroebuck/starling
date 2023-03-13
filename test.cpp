f531303d7eca4c1d816a89c56fdc089a6d489b3e
unknown <kroebuck529@gmail.com>
2023-03-13 15:39:17 -0400

    Implement basic streaming of data from C++ executable to web browser

--- test.cpp
+++ test.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
+
+// read files
+#include <fstream>
+//#include <sstream>
+
+// sleep
+#include <chrono>
+#include <thread>
 
 int main()
 {
-	std::ifstream infile("initial_data.txt");
-	std::string line;
-	while (std::getline(infile, line))
-	{
-		std::cout << line << std::endl;
+	// std::ifstream infile("initial_data.txt");
+	// std::string line;
+	// while (std::getline(infile, line))
+	// {
+	// 	std::cout << line << std::endl;
+	// }
+
+	for (int i = 0; i < 100000; i++) {
+		std::cout << i << '\n';//std::endl;
+		if (i % 10 == 0) std::cout << std::flush;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 
 	return 0;
