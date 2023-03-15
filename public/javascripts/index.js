fc228afaad4b5783f6195f3b50a72bbca552d588
unknown <kroebuck529@gmail.com>
2023-03-15 17:47:07 -0400

    Implement transferring and drawing of data

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,46 @@
-console.log("hello");
+function Body(id, m, x, y, vx, vy) {
+	this.id = id;
+	this.m = m;
+	this.x = x;
+	this.y = y;
+	this.vx = vx;
+	this.vy = vy;
+}
+
+var bodies = [];
+
+function getBody(id) {
+	for (var i = 0; i < bodies.length; i++) {
+		if (bodies[i].id == id) return bodies[i];
+	}
+	return null;
+}
+
+function draw(canvas, x, y, xNew, yNew) {
+	var ctx = canvas.getContext("2d");
+	ctx.moveTo(x, y);
+	ctx.lineTo(xNew, yNew);
+	ctx.stroke();
+}
 
 document.addEventListener('DOMContentLoaded', function() {
 	var socket = io();
 
 	socket.on('data', (res) => {
-		console.log(res.data);
+		res = JSON.parse(res);
+		res.forEach((item) => {
+			var body = getBody(item.id);
+			if (body == null) {
+				body = new Body(item.id, item.m, item.x, item.y, item.vx, item.vy);
+				bodies.push(body);
+			} else {
+				var canvas = document.getElementById('canvas');
+				draw(canvas, body.x, body.y, item.x, item.y);
+			}
+		});
 	});
 
 	document.getElementById('start_simulation').onclick = () => {
 		socket.emit('startSimulation', {});
 	};
 });
-
-// socket call to server
-	// have server run cpp & send back
-
-// listen for cpp data
-	// display
