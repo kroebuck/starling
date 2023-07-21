2cd3c0714eb8f71e313337c9b8c84314146f64c0
Kevin Roebuck <kroebuck529@gmail.com>
2023-07-21 14:16:55 -0400

    add files to be deleted

--- barneshutree.cpp
+++
@@ -1,5 +0,0 @@
-#include "barneshutree.h"
-
-BarnesHutTree::BarnesHutTree(Octant* o) {
-    _octant = o;
-}
--- particle.cpp
+++
@@ -1,6 +0,0 @@
-#include "particle.h"
-
-Particle::Particle() = default;
-
-Particle::Particle(int id, double mass, Vector3 position, Vector3 velocity, Vector3 acceleration) :
-    id(id), mass(mass), position(position), velocity(velocity), acceleration(acceleration) {}
